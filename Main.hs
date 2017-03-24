8db454535082d475d726e9afcff5eafe0b32ccfb
zhangyutong926 <yotochang@gmail.com>
2017-03-24 09:12:45 +0800

    Homepage & WeChat OpenID & Persistent Info

--- Main.hs
+++ Main.hs
@@ -31,6 +31,7 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RankNTypes #-}
 
 module Main where
 
@@ -41,45 +42,82 @@ import Yesod.Core
 import Text.Hamlet
 import Text.Blaze.Html.Renderer.String
 
+import qualified Network.HTTP.Types as H
+
 -- Application routing/database/resource configuration, no modification unless
 -- you are extremely certain what they're and what are their function.
-data HsacnuLockControl = HsacnuLockControl
+data HsacnuLockControl =
+  HsacnuLockControlWithConfig {
+    wcOpenId :: String,
+    dbUrl :: String,
+    dbUser :: String,
+    dbPswd :: String
+  } | HsacnuLockControl
+-- $(makeLenses ''HsacnuLockControlWithConfig)
 
 $(mkYesod "HsacnuLockControl" [parseRoutes|
 / HomeR GET
+/wechat_openid_redirect WeChatOpenIDRedirectR GET
+/wechat_openid_callback WeChatOpenIDCallback GET
 |])
 
 instance Yesod HsacnuLockControl
-
-htmlTest :: Html
-htmlTest = [shamlet|
-<p>Greetings.
-<p>System functioning normally.
-<p>Please designate your intention.
-|]
 
 -- UserInfo Enum Sex
 data Sex = Male | Female | Unspecified deriving (Eq, Enum, Show, Read)
 
 -- UserInfo Primary ADT
-data UserInfo = RequesterUser {
-                                openId :: String,
-                                nickName :: String,
-                                sex :: Sex,
-                                province :: String,
-                                city :: String,
-                                country :: String,
-                                headImageUrl :: String,
-                                privilege :: [String],
-                                unionId :: String
-                              }
-              | ResponserUser {
-                                placeholder :: ()
-                              } deriving (Eq, Show, Read)
+data UserInfo =
+  RequesterUser {
+    openId :: String,
+    nickName :: String,
+    sex :: Sex,
+    province :: String,
+    city :: String,
+    country :: String,
+    headImageUrl :: String,
+    privilege :: [String],
+    unionId :: String
+  } | ResponserUser {
+    placeholder :: () -- TODO Repsonser
+  } deriving (Eq, Show, Read)
+
+jQueryW :: Widget
+jQueryW = addScriptRemote "https://code.jquery.com/jquery-3.2.1.min.js"
+
+-- Home page Widget (welcome info and continue button)
+homePageP :: Widget
+homePageP = do
+  setTitle "HsacnuLockControl - Home Page"
+  toWidget [hamlet|
+    <h1>HsacnuLockControl - Home Page
+    <p>
+      Welcome to the Hsacnu Lock Control. #
+      This system is built by Evrika Logismos Lamda based on Haskell Yesod Web Framework. #
+      The owner of the system is Jingze Zhang, the commissioner of the project #
+      The purpose of this system is to help you open the lock of valuable properties in the campus. #
+    <p>
+      Now you're going to use your WeChat OpenID so that we can identify yourself. #
+      And after that, you'll receive some options that was reserved in the database. #
+      Those options are the representation of specific locks that is active for the moment. #
+      Then you're going to choose one of them and leave some optional additional message. #
+      Once you've done that, submit your request and the system will send your identity and your message to the proper administrator that possess the premission to grant the lock opening. #
+      And when that person received the message on his/her terminal, he/she will make a decision in 5 minutes about whether or not to open the lock, while the mean time, you're going to stay one the pending page and wait for status refresh. #
+      If the administrator didn't response in 5 minutes, then your request is outdated and invalidated and you have to restart the procedure for the beginning, which means, here. #
+      Also, if you shut the pending webpage accidentally, don't worry, you can still go back to the home page and provide the WeChat OpenID again, then you'll be redirect to the pending page. #
+    <p> As far as I can tell, the system is secured and privacy-friendly, and that means I, as the developer of the system, shall not positively collect any of you personel data, including the OpenID itself. They'll be destroyed immediately after the requesting procedure was finished or outdated.
+    <p>
+      And now, please provide your WeChat OpenID using the following hyper-link.<br>
+      <a href=@{WeChatOpenIDRedirectR}>WeChat OpenID Validation
+  |]
 
 -- Handler is the controller of web application (controller in MVC)
 getHomeR :: Handler Html
-getHomeR = defaultLayout [whamlet|Hello World!|]
+getHomeR = defaultLayout homePageP
+
+-- Redirect the user to the WeChat OpenID Login Page
+getWeChatOpenIDRedirectR :: Handler ()
+getWeChatOpenIDRedirectR = redirect ("https://google.com" :: String)
 
 -- Default web-server port, you may modify it if you want to
 defaultPort :: Int
